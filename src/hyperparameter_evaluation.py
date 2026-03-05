bc57a1b9bde196970cc5a8fed457de3c5d526416
TeddyVDobreva <t.v.dobreva@student.rug.nl>
2026-03-05 12:27:14 +0100

    env

---
+++ src/hyperparameter_evaluation.py
@@ -0,0 +1,115 @@
+from pathlib import Path
+from typing import Any
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.svm import LinearSVC
+
+
+def do_hyperparameter_evaluation(
+    model: LinearSVC | LogisticRegression,
+    hyperparameter1: dict[str, list],
+    hyperparameter2: dict[str, list],
+    X_train: Any,
+    labels_train: pd.Series,
+    X_validation: Any,
+    labels_validation: pd.Series,
+    **kwargs: Any,
+) -> None:
+    """
+    Function iterates over the provided hyperparameters, trains a new model
+    for every combination, evaluates it, then stores the result in a 2D
+    accuracy matrix. The model skips the unsupported combinations of
+    hyperparameters.
+
+    Arguments:
+        model: LinearSVC | LogisticRegression- The model which will be
+            trained and evaluated.
+        hyperparameter1: dict[str, list]- Dictionary containing the 1st
+            hyperparameter name.
+        hyperparameter2: dict[str, list]- Dictionary containing the 2nd
+            hyperparameter name.
+        X_train: Any- Training matrix.
+        labels_train: pd.Series- Training labels.
+        X_validation: Any- Validation matrix.
+        labels_validation: pd.Series- Validation labels.
+        **kwargs: Any- Additional individual arguments necessary for each
+            model.
+
+    Returns: None
+    """
+    hp1_name = list(hyperparameter1.keys())[0]
+    hp2_name = list(hyperparameter2.keys())[0]
+
+    accuracy_matrix = np.full(
+        (len(hyperparameter1[hp1_name]), len(hyperparameter2[hp2_name])), np.nan
+    )
+
+    for i, hp1 in enumerate(hyperparameter1[hp1_name]):
+        print(f"Current {hp1_name}: {hp1}")
+
+        for j, hp2 in enumerate(hyperparameter2[hp2_name]):
+            print(f"Current {hp2_name} {hp2}")
+            try:
+                hyperparameter_dic = {hp1_name: hp1, hp2_name: hp2}
+
+                model_to_tune = model(random_state=67, **hyperparameter_dic, **kwargs)
+
+                model_to_tune.fit(X_train, labels_train)
+
+                validation_predictions = model_to_tune.predict(X_validation)
+                validation_accuracy = accuracy_score(
+                    labels_validation, validation_predictions
+                )
+
+                accuracy_matrix[i, j] = validation_accuracy
+            except:
+                print("Skip unsupported combination of hyperparameters")
+        print()
+
+    make_heatmap(accuracy_matrix, hyperparameter1, hyperparameter2)
+
+
+def make_heatmap(
+    accuracy_matrix: np.ndarray,
+    hyperparameter1: dict[str, list],
+    hyperparameter2: dict[str, list],
+) -> None:
+    """
+    Function presents the accuracy matrix by using a heatmap, which shows how
+    performance varies for the models when using different hyperparameters.
+
+    Arguments:
+        accuracy_matrix: np.ndarray: 2D matrix containing accuracies for
+            each hyperparameter combination.
+       hyperparameter1: dict[str, list]- Dictionary containing the 1st
+            hyperparameter name.
+        hyperparameter2: dict[str, list]- Dictionary containing the 2nd
+            hyperparameter name.
+
+    Returns: None
+    """
+    Path("plots").mkdir(exist_ok=True)
+
+    plt.figure(figsize=(8, 6))
+    hp1_name = list(hyperparameter1.keys())[0]
+    hp2_name = list(hyperparameter2.keys())[0]
+
+    sns.heatmap(
+        accuracy_matrix,
+        annot=True,
+        fmt=".3f",
+        yticklabels=hyperparameter1[hp1_name],
+        xticklabels=hyperparameter2[hp2_name],
+        cmap="viridis",
+    )
+
+    plt.title("Heatmap")
+    plt.ylabel(hp1_name)
+    plt.xlabel(hp2_name)
+    plt.savefig(f"plots/heatmap_{hp1_name}_{hp2_name}")
+    plt.close()
